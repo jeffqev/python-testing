5cfff8aa16cb06128962ca363f6c438cc9d099aa
Jefferson Oña <67077796+jeffqev@users.noreply.github.com>
2023-05-30 09:00:33 -0500

    naming and AAA

--- test/unit/calculator_unit_test.py
+++ test/unit/calculator_unit_test.py
@@ -3,20 +3,35 @@ from pytest_mock import MockFixture
 
 from app import Calculator
 
-def test_addition():
-    calculator = Calculator()
-    assert calculator.add(1, 1) == 2
-
-def test_division():
-    calculator = Calculator()
-    assert calculator.divide(4, 2) == 2
-
-def test_division_raises_exception(mocker: MockFixture):
+def test_addition_returns_the_correct_result_when_two_number_are_passed():
+    #Arrange
+    number_1 = 1
+    number_2 = 1
+    expected_result = 2
     calculator = Calculator()
 
+    #Act
+    actual_result = calculator.add(number_1, number_2)
+
+    #Assert
+    assert  actual_result == expected_result
+
+def test_division_returns_the_correct_result_when_two_number_are_passed():
+    number_1 = 4
+    number_2 = 2
+    expected_result = 2
+    calculator = Calculator()
+
+    actual_result = calculator.divide(number_1, number_2)
+
+    assert actual_result == expected_result
+
+def test_division_raises_an_exception_when_is_divided_by_zero(mocker: MockFixture):
+    calculator = Calculator()
     log = mocker.patch('app.logs.logging_msg')
 
     with pytest.raises(ValueError) as exception_info:
         calculator.divide(4, 0)
+        
         assert str(exception_info) == "Can not divide by zero!"
         log.assert_called_once_with("Can not divide by zero!")
--- test/unit/payroll_unit_test.py
+++ test/unit/payroll_unit_test.py
@@ -3,35 +3,45 @@ from pytest_mock import MockFixture
 from app import Payroll
 from app.calculator import Calculator
 
-def test_calculate_salary (): 
+def test_calculate_salary_calculate_the_salary_when_data_is_correct(): 
     salary = 1000
     bonus = 100
     taxes = 2
+    expected_salary = (salary + bonus) / taxes
     payroll = Payroll()
-    result_salary = payroll.calculate_salary(salary, bonus, taxes)
-    assert result_salary == (salary + bonus) / taxes
+
+    actual_salary = payroll.calculate_salary(salary, bonus, taxes)
+    
+    assert actual_salary == expected_salary
 
 
-def test_calculate_salary_2 (mocker: MockFixture): 
+def test_calculate_salary_calls_the_calculator_functions(mocker: MockFixture): 
     salary = 1000
     bonus = 100
     taxes = 2
-    payroll = Payroll()
+    expected_salary = (salary + bonus) / taxes
     add = mocker.patch('app.calculator.Calculator.add')
     add.return_value = salary + bonus
-    divide = mocker.patch('app.calculator.Calculator.divide', return_value=(salary + bonus) / taxes)
-    salary_result = payroll.calculate_salary(salary, bonus, taxes)
-    assert salary_result == ((salary + bonus) / taxes)
+    divide = mocker.patch('app.calculator.Calculator.divide', return_value=expected_salary)
+    payroll = Payroll()
+    
+    actual_salary = payroll.calculate_salary(salary, bonus, taxes)
+    
+    assert actual_salary == expected_salary
     add.assert_called_once_with(salary, bonus)
     divide.assert_called_once_with((salary + bonus), taxes)
 
-def test_liquidation(mocker: MockFixture):
+def test_liquidation_returns_correct_liquidation(mocker: MockFixture):
     employee_id = 1
     salary = 1000
     months_worked = 12
-    payroll = Payroll()
+    expected_liquidation = salary + (salary * months_worked)
+    mock_employee_data = {'months_worked': months_worked}
     get_employee_data = mocker.patch('app.Payroll.get_employee_data')
-    get_employee_data.return_value = {'months_worked': months_worked}
+    get_employee_data.return_value = mock_employee_data
+    payroll = Payroll()
+
     liquidation_result = payroll.calculate_liquidation(employee_id, salary)
-    assert liquidation_result == (salary + (salary * months_worked))
+
+    assert liquidation_result == expected_liquidation
     get_employee_data.assert_called_once_with(employee_id)
